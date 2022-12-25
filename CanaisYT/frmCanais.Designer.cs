2f22991cb856bfada94cb7a9dfda1da87c65d707
USE104 <jose.hssilva97@gmail.com>
2022-12-25 16:58:43 -0300

    Add project files.

---
+++ CanaisYT/frmCanais.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace CanaisYT
+{
+    partial class frmCanais
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCanais));
+            this.btnTecmundo = new System.Windows.Forms.Button();
+            this.btnOlharDigital = new System.Windows.Forms.Button();
+            this.btnSafesrc = new System.Windows.Forms.Button();
+            this.grpNoticiaisTecnologia = new System.Windows.Forms.GroupBox();
+            this.grpAnalisesdeHardware = new System.Windows.Forms.GroupBox();
+            this.btnGarimpandoagambiara = new System.Windows.Forms.Button();
+            this.btnPCdoFafa = new System.Windows.Forms.Button();
+            this.btnTecnoArt = new System.Windows.Forms.Button();
+            this.btnMW = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.grpNoticiaisTecnologia.SuspendLayout();
+            this.grpAnalisesdeHardware.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTecmundo
+            // 
+            this.btnTecmundo.BackColor = System.Drawing.SystemColors.Control;
+            this.btnTecmundo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTecmundo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTecmundo.Image = ((System.Drawing.Image)(resources.GetObject("btnTecmundo.Image")));
+            this.btnTecmundo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTecmundo.Location = new System.Drawing.Point(6, 23);
+            this.btnTecmundo.Name = "btnTecmundo";
+            this.btnTecmundo.Size = new System.Drawing.Size(117, 121);
+            this.btnTecmundo.TabIndex = 7;
+            this.btnTecmundo.Text = "Tecmundo";
+            this.btnTecmundo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnTecmundo.UseVisualStyleBackColor = false;
+            this.btnTecmundo.Click += new System.EventHandler(this.btnTecmundo_Click);
+            // 
+            // btnOlharDigital
+            // 
+            this.btnOlharDigital.BackColor = System.Drawing.SystemColors.Control;
+            this.btnOlharDigital.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOlharDigital.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOlharDigital.Image = ((System.Drawing.Image)(resources.GetObject("btnOlharDigital.Image")));
+            this.btnOlharDigital.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnOlharDigital.Location = new System.Drawing.Point(132, 23);
+            this.btnOlharDigital.Name = "btnOlharDigital";
+            this.btnOlharDigital.Size = new System.Drawing.Size(117, 121);
+            this.btnOlharDigital.TabIndex = 8;
+            this.btnOlharDigital.Text = "OlharDigital";
+            this.btnOlharDigital.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnOlharDigital.UseVisualStyleBackColor = false;
+            this.btnOlharDigital.Click += new System.EventHandler(this.btnOlharDigital_Click);
+            // 
+            // btnSafesrc
+            // 
+            this.btnSafesrc.BackColor = System.Drawing.SystemColors.Control;
+            this.btnSafesrc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSafesrc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSafesrc.Image = ((System.Drawing.Image)(resources.GetObject("btnSafesrc.Image")));
+            this.btnSafesrc.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSafesrc.Location = new System.Drawing.Point(255, 23);
+            this.btnSafesrc.Name = "btnSafesrc";
+            this.btnSafesrc.Size = new System.Drawing.Size(117, 121);
+            this.btnSafesrc.TabIndex = 10;
+            this.btnSafesrc.Text = "Safesrc";
+            this.btnSafesrc.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSafesrc.UseVisualStyleBackColor = false;
+            this.btnSafesrc.Click += new System.EventHandler(this.btnSafesrc_Click);
+            // 
+            // grpNoticiaisTecnologia
+            // 
+            this.grpNoticiaisTecnologia.Controls.Add(this.btnSafesrc);
+            this.grpNoticiaisTecnologia.Controls.Add(this.btnOlharDigital);
+            this.grpNoticiaisTecnologia.Controls.Add(this.btnTecmundo);
+            this.grpNoticiaisTecnologia.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.grpNoticiaisTecnologia.Location = new System.Drawing.Point(100, 174);
+            this.grpNoticiaisTecnologia.Name = "grpNoticiaisTecnologia";
+            this.grpNoticiaisTecnologia.Size = new System.Drawing.Size(380, 151);
+            this.grpNoticiaisTecnologia.TabIndex = 15;
+            this.grpNoticiaisTecnologia.TabStop = false;
+            this.grpNoticiaisTecnologia.Text = "Noticiais de Tecnologia";
+            // 
+            // grpAnalisesdeHardware
+            // 
+            this.grpAnalisesdeHardware.Controls.Add(this.btnGarimpandoagambiara);
+            this.grpAnalisesdeHardware.Controls.Add(this.btnPCdoFafa);
+            this.grpAnalisesdeHardware.Controls.Add(this.btnTecnoArt);
+            this.grpAnalisesdeHardware.Controls.Add(this.btnMW);
+            this.grpAnalisesdeHardware.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.grpAnalisesdeHardware.Location = new System.Drawing.Point(12, 3);
+            this.grpAnalisesdeHardware.Name = "grpAnalisesdeHardware";
+            this.grpAnalisesdeHardware.Size = new System.Drawing.Size(577, 149);
+            this.grpAnalisesdeHardware.TabIndex = 16;
+            this.grpAnalisesdeHardware.TabStop = false;
+            this.grpAnalisesdeHardware.Text = "Análises de Hardware";
+            // 
+            // btnGarimpandoagambiara
+            // 
+            this.btnGarimpandoagambiara.BackColor = System.Drawing.SystemColors.Control;
+            this.btnGarimpandoagambiara.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGarimpandoagambiara.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGarimpandoagambiara.Image = ((System.Drawing.Image)(resources.GetObject("btnGarimpandoagambiara.Image")));
+            this.btnGarimpandoagambiara.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnGarimpandoagambiara.Location = new System.Drawing.Point(385, 19);
+            this.btnGarimpandoagambiara.Name = "btnGarimpandoagambiara";
+            this.btnGarimpandoagambiara.Size = new System.Drawing.Size(178, 121);
+            this.btnGarimpandoagambiara.TabIndex = 18;
+            this.btnGarimpandoagambiara.Text = "Garimpando Gambiara";
+            this.btnGarimpandoagambiara.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnGarimpandoagambiara.UseVisualStyleBackColor = false;
+            this.btnGarimpandoagambiara.Click += new System.EventHandler(this.btnGarimpandoagambiara_Click);
+            // 
+            // btnPCdoFafa
+            // 
+            this.btnPCdoFafa.BackColor = System.Drawing.SystemColors.Control;
+            this.btnPCdoFafa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPCdoFafa.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPCdoFafa.Image = ((System.Drawing.Image)(resources.GetObject("btnPCdoFafa.Image")));
+            this.btnPCdoFafa.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnPCdoFafa.Location = new System.Drawing.Point(262, 19);
+            this.btnPCdoFafa.Name = "btnPCdoFafa";
+            this.btnPCdoFafa.Size = new System.Drawing.Size(117, 121);
+            this.btnPCdoFafa.TabIndex = 17;
+            this.btnPCdoFafa.Text = "PC do Fafa";
+            this.btnPCdoFafa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPCdoFafa.UseVisualStyleBackColor = false;
+            this.btnPCdoFafa.Click += new System.EventHandler(this.btnPCdoFafa_Click);
+            // 
+            // btnTecnoArt
+            // 
+            this.btnTecnoArt.BackColor = System.Drawing.SystemColors.Control;
+            this.btnTecnoArt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTecnoArt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTecnoArt.Image = ((System.Drawing.Image)(resources.GetObject("btnTecnoArt.Image")));
+            this.btnTecnoArt.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTecnoArt.Location = new System.Drawing.Point(139, 19);
+            this.btnTecnoArt.Name = "btnTecnoArt";
+            this.btnTecnoArt.Size = new System.Drawing.Size(117, 121);
+            this.btnTecnoArt.TabIndex = 16;
+            this.btnTecnoArt.Text = "TecnoArt";
+            this.btnTecnoArt.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnTecnoArt.UseVisualStyleBackColor = false;
+            this.btnTecnoArt.Click += new System.EventHandler(this.btnTecnoArt_Click);
+            // 
+            // btnMW
+            // 
+            this.btnMW.BackColor = System.Drawing.SystemColors.Control;
+            this.btnMW.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMW.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMW.Image = ((System.Drawing.Image)(resources.GetObject("btnMW.Image")));
+            this.btnMW.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnMW.Location = new System.Drawing.Point(6, 19);
+            this.btnMW.Name = "btnMW";
+            this.btnMW.Size = new System.Drawing.Size(127, 121);
+            this.btnMW.TabIndex = 15;
+            this.btnMW.Text = "MW Informática";
+            this.btnMW.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMW.UseVisualStyleBackColor = false;
+            this.btnMW.Click += new System.EventHandler(this.btnMW_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(7, 158);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 10);
+            this.panel1.TabIndex = 17;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnSair.Location = new System.Drawing.Point(12, 325);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(51, 23);
+            this.btnSair.TabIndex = 18;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmCanais
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(603, 360);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.grpAnalisesdeHardware);
+            this.Controls.Add(this.grpNoticiaisTecnologia);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCanais";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Canais do Youtube";
+            this.Load += new System.EventHandler(this.frmCanais_Load);
+            this.grpNoticiaisTecnologia.ResumeLayout(false);
+            this.grpAnalisesdeHardware.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTecmundo;
+        private System.Windows.Forms.Button btnOlharDigital;
+        private System.Windows.Forms.Button btnSafesrc;
+        private System.Windows.Forms.GroupBox grpNoticiaisTecnologia;
+        private System.Windows.Forms.GroupBox grpAnalisesdeHardware;
+        private System.Windows.Forms.Button btnGarimpandoagambiara;
+        private System.Windows.Forms.Button btnPCdoFafa;
+        private System.Windows.Forms.Button btnTecnoArt;
+        private System.Windows.Forms.Button btnMW;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ CanaisYT/frmCanais.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CanaisYT
+{
+    public partial class frmCanais : Form
+    {
+        public frmCanais()
+        {
+            InitializeComponent();
+            
+        }
+
+        const int MF_BYCOMMAND = 0X400;
+        [DllImport("user32")]
+        static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [DllImport("user32")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32")]
+        static extern int GetMenuItemCount(IntPtr hWnd);
+
+
+        private void frmCanais_Load(object sender, EventArgs e)
+        {
+            IntPtr hMenu = GetSystemMenu(this.Handle, false);
+            int MenuCount = GetMenuItemCount(hMenu) - 1;
+            RemoveMenu(hMenu, MenuCount, MF_BYCOMMAND);
+        }
+
+
+        private void btnMW_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@MWInformatica/videos");
+            pStart.Start();
+        }
+
+        private void btnTecnoArt_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@tecnoart101/videos");
+            pStart.Start();
+
+        }
+
+        private void btnGarimpandoagambiara_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@Garimpandoagambiarra/videos");
+            pStart.Start();
+        }
+
+        private void btnTecmundo_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@tecmundo/videos");
+            pStart.Start();
+        }
+
+
+        private void btnPCdoFafa_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@pcdofafa/videos");
+            pStart.Start();
+        }
+        private void btnOlharDigital_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@olhardigital/videos");
+            pStart.Start();
+        }
+        private void btnSafesrc_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process pStart = new System.Diagnostics.Process();
+            pStart.StartInfo = new System.Diagnostics.ProcessStartInfo(@"https://www.youtube.com/@safesrc/videos");
+            pStart.Start();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+    
+}
+
+
+
+
+
+
+
